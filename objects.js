a5ec36bffd6deeabb4158bc8a9b5d0e3ace4fc85
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 00:07:52 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { 'Will Smith' : 'Big Willie Style', 'The Killers' : 'Mr. Brightside' };
+
+function updatePlaylist(obj, artistName, song) {
+  var newPlaylist = Object.assign({}, obj, {[artistName] : song});
+  return newPlaylist;
+}
+
+function removeFromPlaylist(obj, artistName) {
+    var newPlaylist = Object.assign({}, obj);
+  delete newPlaylist[artistName];
+  return newPlaylist;
+}
+
